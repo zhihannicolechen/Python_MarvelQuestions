f182702f4c6d41fa6910084154bf1a407a8a309b
Chen Zhihan <ZHco13@users.noreply.github.com>
2021-12-09 22:19:17 -0500

    complete the content for the quiz

--- components/quizQuestions.py
+++ components/quizQuestions.py
@@ -1,13 +1,52 @@
 questions = {
     "q1" : {
-        "question": "Do you live on earth? ",
-        "yes" : 0,
-        "no" : 10
+        "question": "Do you live on earth? "
+                    "Y / N : ",
+        "yes" : 10,
+        "no" : 0
     },
 
     "q2" : {
-        "question" : "Are you human? ",
-        "yes" : 0,
-        "no" : 10
-    }
+        "question" : "Are you a male? "
+                    "Y / N : ",
+        "yes" : 10,
+        "no" : 0
+    },
+
+    "q3" : {
+        "question" : "Are you a hero? "
+                    "Y / N : ",
+
+        "yes" : 10,
+        "no" : 0
+    },
+
+    "q4" : {
+        "question": "Are you famous because of your battle equipment? "
+                    "Y / N : ",
+        "yes" : 10,
+        "no" : 0
+    },
+
+    "q5" : {
+        "question" : "Does your heart always insist on justice? "
+                    "Y / N : ",
+        "yes" : 10,
+        "no" : 0
+    },
+
+    "q6" : {
+        "question" : "Are you always able to keep your sanity? "
+                    "Y / N : ",
+        "yes" : 10,
+        "no" : 0
+    },
+
+    "q7" : {
+        "question" : "Do you have an extremely powerful body? "
+                    "Y / N : ",
+        "yes" : 10,
+        "no" : 0
+    },
+
 }
--- components/quizTally.py
+++ components/quizTally.py
@@ -1,13 +1,27 @@
 from components import vars
+from PIL import Image
+from emoji import emojize
 
 
 def total(value):
     # do some logic to see which character you selected
 
-    if value <= 10:
-        vars.character = vars.characters[0]
+    if value <= 20:
+        print("You are most similar with ", vars.characters[2], "!")
+        im = Image.open("Loki.jpg")
+        im.show()
 
-        print("It's " + vars.character)
-        # add some emoji icons, or show the character image using the Pillow package
+    elif value <= 30:
+        print("You are most similar with ", vars.characters[1],"!")
+        im = Image.open("BlackWidow.jpg")
+        im.show()
 
-    
+    elif value <= 40:
+        print ("You are most similar with ", vars.characters[0],"!")
+        im = Image.open("hulk.png")
+        im.show()
+
+    elif value <= 70:
+        print ("You are most similar with ", vars.characters[3],"!")
+        im = Image.open("Iron_man.jpg")
+        im.show()
